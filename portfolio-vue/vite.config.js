907ca8359326952951cdff99827ea0bf39828fcb
thinkbruno <brunoramos90@gmail.com>
2026-03-19 15:47:59 -0300

    vite dnv dnv

--- portfolio-vue/vite.config.js
+++ portfolio-vue/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: './', // <--- base relativa para build local + serve + GitHub Pages
   plugins: [vue()],
   root: resolve(__dirname),
   build: {
