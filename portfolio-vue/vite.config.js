c15f6b398c44657f070bee0aba98da8c9e706450
thinkbruno <brunoramos90@gmail.com>
2026-03-19 15:36:34 -0300

    ajuste no vite

--- portfolio-vue/vite.config.js
+++ portfolio-vue/vite.config.js
@@ -3,12 +3,13 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/thinkbruno.github.io/', // <- ESSENCIAL para GitHub Pages
   plugins: [vue()],
-  root: resolve(__dirname), // raiz do projeto
+  root: resolve(__dirname),
   build: {
-    outDir: 'dist',         // pasta de saída
+    outDir: 'dist',
     rollupOptions: {
-      input: resolve(__dirname, 'index.html'), // ponto de entrada
+      input: resolve(__dirname, 'index.html'),
     },
   },
 })
